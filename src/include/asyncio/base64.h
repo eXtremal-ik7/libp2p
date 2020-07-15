a7a159ae3ba799561da690c89984f9aee9074a08
eXtremal-ik7 <eXtremal.ik7@gmail.com>
2020-07-16 01:03:20 +0300

    base64: allow use from C++

--- src/include/asyncio/base64.h
+++ src/include/asyncio/base64.h
@@ -1,6 +1,10 @@
 #ifndef __ASYNCIO_BASE64_H_
 #define __ASYNCIO_BASE64_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -8,5 +12,9 @@ size_t base64GetDecodeLength(const char *in);
 size_t base64getEncodeLength(size_t len);
 size_t base64Decode(uint8_t *out, const char *in);
 size_t base64Encode(char *out, const uint8_t *in, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //__ASYNCIO_BASE64_H_
